da745c9dc0a28c7b2e4e8a8e0fb043489ccac9ea
Matthew Eshleman <matthew@covemountainsoftware.com>
2020-04-26 17:37:05 -0500

    ongoing clean up

--- services/lockCtrlService/src/lockCtrlService.cpp
+++ services/lockCtrlService/src/lockCtrlService.cpp
@@ -1,3 +1,26 @@
+/*
+MIT License
+
+Copyright (c) <2019-2020> <Matthew Eshleman - https://covemountainsoftware.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 #include "cmsFlatStateMachine.hpp"
 #include "lockCtrlService.hpp"
@@ -110,35 +133,10 @@ LockCtrlService::StateRtn LockCtrlService::SelfTest(const ServiceEventType* cons
     StateRtn rtn;
     switch (event->signal)
     {
-    case cms::SM_ENTER: {
-        LockCtrlSelfTestResult result;
-        bool ok = LockCtrlSelfTest(&result);
-        if (ok && (result == LOCK_CTRL_SELF_TEST_PASSED))
-        {
-            NotifySelfTestResult(SelfTestResult::PASS);
-        }
-        else
-        {
-            NotifySelfTestResult(SelfTestResult::FAIL);
-        }
-
-        //remind self to transition back to
-        //history per this service's requirements
-        //note the use of "PostUrgent" as per:
-        //
-        // https://covemountainsoftware.com/2020/03/08/uml-statechart-handling-errors-when-entering-a-state/
-        //
-        if (mHistory == &LockCtrlService::Unlocked)
-        {
-            PostUrgent(REQUEST_UNLOCKED);
-        }
-        else
-        {
-            PostUrgent(REQUEST_LOCKED);
-        }
+    case cms::SM_ENTER:
+        PerformSelfTest();
         rtn = Handled();
         break;
-    }
     case REQUEST_LOCKED:
         rtn = TransitionTo(&LockCtrlService::Locked);
         break;
@@ -154,6 +152,34 @@ LockCtrlService::StateRtn LockCtrlService::SelfTest(const ServiceEventType* cons
     }
 
     return rtn;
+}
+void LockCtrlService::PerformSelfTest()
+{
+    LockCtrlSelfTestResult result;
+    bool ok = LockCtrlSelfTest(&result);
+    if (ok && (result == LOCK_CTRL_SELF_TEST_PASSED))
+    {
+        NotifySelfTestResult(SelfTestResult::PASS);
+    }
+    else
+    {
+        NotifySelfTestResult(SelfTestResult::FAIL);
+    }
+
+    //remind self to transition back to
+    //history per this service's requirements
+    //note the use of "PostUrgent" as per:
+    //
+    // https://covemountainsoftware.com/2020/03/08/uml-statechart-handling-errors-when-entering-a-state/
+    //
+    if (mHistory == &LockCtrlService::Unlocked)
+    {
+        PostUrgent(REQUEST_UNLOCKED);
+    }
+    else
+    {
+        PostUrgent(REQUEST_LOCKED);
+    }
 }
 
 void LockCtrlService::NotifyChangedState(LockCtrlService::LockState state)
